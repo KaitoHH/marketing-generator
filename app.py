dc1ed04687d8d265dea2906e40a174959cd5b0ea
KaitoHH <hh.kaito@gmail.com>
2020-04-25 23:52:18 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from gtts import gTTS
+from util import gen_text
+from pydub import AudioSegment
+import base64
+import os
+from flask import Flask, render_template, send_file, request
+
+app = Flask(__name__)
+app.jinja_env.variable_start_string = '[['
+app.jinja_env.variable_end_string = ']]'
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/generate')
+def generate():
+    what = request.args.get('what')
+    how = request.args.get('how')
+    fact = request.args.get('fact')
+    name = '{0}{1}.mp3'.format(what, how)
+    gTTS(text=gen_text(what, how, fact), lang='zh-cn', slow=False).save(name)
+    target = AudioSegment.from_file(name)
+    bgm = AudioSegment.from_file('resource/trip.m4a')
+    ret = target.overlay(bgm)
+    ret.export(name)
+    enc = base64.b64encode(open(name, "rb").read())
+    os.remove(name)
+    src = b'data:audio/mpeg;base64,' + enc
+    return src
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
